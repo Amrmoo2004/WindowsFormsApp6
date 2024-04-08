dfd22367cd4bf1d6d5ee5684307925cacda8adba
amr <amr@AMR>
2024-04-08 22:02:11 +0200

    Add project files.

---
+++ WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.A9 = new System.Windows.Forms.Button();
+            this.A8 = new System.Windows.Forms.Button();
+            this.A6 = new System.Windows.Forms.Button();
+            this.A5 = new System.Windows.Forms.Button();
+            this.A7 = new System.Windows.Forms.Button();
+            this.A4 = new System.Windows.Forms.Button();
+            this.A3 = new System.Windows.Forms.Button();
+            this.A2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.A1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // A9
+            // 
+            this.A9.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A9.Location = new System.Drawing.Point(265, 230);
+            this.A9.Name = "A9";
+            this.A9.Size = new System.Drawing.Size(75, 54);
+            this.A9.TabIndex = 2;
+            this.A9.Text = "?";
+            this.A9.UseVisualStyleBackColor = true;
+            this.A9.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // A8
+            // 
+            this.A8.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A8.Location = new System.Drawing.Point(135, 230);
+            this.A8.Name = "A8";
+            this.A8.Size = new System.Drawing.Size(75, 54);
+            this.A8.TabIndex = 3;
+            this.A8.Text = "?";
+            this.A8.UseVisualStyleBackColor = true;
+            this.A8.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // A6
+            // 
+            this.A6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A6.Location = new System.Drawing.Point(265, 123);
+            this.A6.Name = "A6";
+            this.A6.Size = new System.Drawing.Size(75, 54);
+            this.A6.TabIndex = 4;
+            this.A6.Text = "?";
+            this.A6.UseVisualStyleBackColor = true;
+            this.A6.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // A5
+            // 
+            this.A5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A5.Location = new System.Drawing.Point(135, 123);
+            this.A5.Name = "A5";
+            this.A5.Size = new System.Drawing.Size(75, 54);
+            this.A5.TabIndex = 5;
+            this.A5.Text = "?";
+            this.A5.UseVisualStyleBackColor = true;
+            this.A5.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // A7
+            // 
+            this.A7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A7.Location = new System.Drawing.Point(12, 230);
+            this.A7.Name = "A7";
+            this.A7.Size = new System.Drawing.Size(75, 54);
+            this.A7.TabIndex = 6;
+            this.A7.Text = "?";
+            this.A7.UseVisualStyleBackColor = true;
+            this.A7.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // A4
+            // 
+            this.A4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A4.Location = new System.Drawing.Point(12, 123);
+            this.A4.Name = "A4";
+            this.A4.Size = new System.Drawing.Size(75, 54);
+            this.A4.TabIndex = 7;
+            this.A4.Text = "?";
+            this.A4.UseVisualStyleBackColor = true;
+            this.A4.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // A3
+            // 
+            this.A3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A3.Location = new System.Drawing.Point(265, 23);
+            this.A3.Name = "A3";
+            this.A3.Size = new System.Drawing.Size(75, 54);
+            this.A3.TabIndex = 8;
+            this.A3.Text = "?";
+            this.A3.UseVisualStyleBackColor = true;
+            this.A3.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // A2
+            // 
+            this.A2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A2.Location = new System.Drawing.Point(135, 23);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(75, 54);
+            this.A2.TabIndex = 9;
+            this.A2.Text = "?";
+            this.A2.UseVisualStyleBackColor = true;
+            this.A2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(167, 341);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 12;
+            this.textBox1.Click += new System.EventHandler(this.button1_Click);
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(91, 347);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 16);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Winner";
+            this.label1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(43, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "reset";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(207, 415);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // A1
+            // 
+            this.A1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A1.Location = new System.Drawing.Point(12, 23);
+            this.A1.Name = "A1";
+            this.A1.Size = new System.Drawing.Size(75, 54);
+            this.A1.TabIndex = 0;
+            this.A1.Text = "?";
+            this.A1.UseVisualStyleBackColor = true;
+            this.A1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.A2);
+            this.Controls.Add(this.A3);
+            this.Controls.Add(this.A4);
+            this.Controls.Add(this.A7);
+            this.Controls.Add(this.A5);
+            this.Controls.Add(this.A6);
+            this.Controls.Add(this.A8);
+            this.Controls.Add(this.A9);
+            this.Controls.Add(this.A1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button A9;
+        private System.Windows.Forms.Button A8;
+        private System.Windows.Forms.Button A6;
+        private System.Windows.Forms.Button A5;
+        private System.Windows.Forms.Button A7;
+        private System.Windows.Forms.Button A4;
+        private System.Windows.Forms.Button A3;
+        private System.Windows.Forms.Button A2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button A1;
+    }
+}
+
---
+++ WindowsFormsApp6/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        bool turn = true;
+        int count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void check()
+        {
+
+            bool is_wineer = false;
+            if ((A1.Text == A2.Text) && (A2.Text == A3.Text) && (!A1.Enabled))
+            {
+                is_wineer = true;
+            }
+            else if ((A4.Text == A5.Text) && (A5.Text == A6.Text) && (!A4.Enabled))
+            {
+                is_wineer = true;
+            }
+            else if ((A7.Text == A8.Text) && (A8.Text == A9.Text) && (!A6.Enabled))
+            {
+                is_wineer = true;
+            }
+            else if ((A1.Text == A4.Text) && (A4.Text == A7.Text) && (!A1.Enabled))
+            {
+                is_wineer = true;
+            }
+            else if ((A2.Text == A5.Text) && (A5.Text == A8.Text) && (!A2.Enabled))
+            {
+                is_wineer = true;
+            }
+            else if ((A3.Text == A6.Text) && (A6.Text == A9.Text) && (!A3.Enabled))
+            {
+                is_wineer = true;
+            }
+            else if ((A1.Text == A5.Text) && (A5.Text == A9.Text) && (!A1.Enabled))
+            {
+                is_wineer = true;
+            }
+            else if ((A3.Text == A5.Text) && (A5.Text == A7.Text) && (!A3.Enabled))
+            {
+                is_wineer = true;
+            }
+            if (is_wineer)
+            {
+                string winner = "  ";
+                if (turn)
+
+                 
+
+                    winner = "o";
+                else
+                    winner = "x";
+
+                DisableButtons();
+
+                MessageBox.Show("winer is " + winner);
+                textBox1.Text = winner;
+            }
+            }
+         private void DisableButtons()
+            {
+                A1.Enabled = false;
+                A2.Enabled = false;
+                A3.Enabled = false;
+                A4.Enabled = false;
+                A5.Enabled = false;
+                A6.Enabled = false;
+                A7.Enabled = false;
+                A8.Enabled = false;
+                A9.Enabled = false;
+            }
+        private void EnableButtons()
+        {
+            A1.Enabled = true;
+            A2.Enabled = true;
+            A3.Enabled = true;
+            A4.Enabled = true;
+            A5.Enabled = true;
+            A6.Enabled = true;
+            A7.Enabled = true;
+            A8.Enabled = true;
+            A9.Enabled = true;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (turn)
+                b.Text = "x";
+            else
+                b.Text = "o";
+            turn = !turn;
+            b.Enabled = false;
+            check();
+        }
+       
+             
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            A1.Text = A2.Text = A3.Text = A4.Text = A5.Text = A6.Text = A7.Text = A8.Text = A9.Text = null;
+            EnableButtons();
+        }
+    }
+}
